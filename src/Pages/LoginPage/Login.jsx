1d66af478efdf109eae9abe3519a469db443ce43
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-06-01 12:09:32 +0600

    login form added

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,37 +1,55 @@
+import { useFormik } from "formik";
 
 const Login = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("submitted")
+    const validate = (values) => {
+        const errors = {};
+        if(!values.email){
+            errors.email = 'Email is required'
+        }
+        if(!values.password){
+            errors.password = "Password is required"
+        }
+        return errors
     }
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate,
+        onSubmit: (values)=> {
+            console.log(JSON.stringify(values, null, 2));
+        }
+    })
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div className="min-h-screen">
+            <h1 className="text-center text-3xl pt-24">Login Here</h1>
+            <form onSubmit={formik.handleSubmit} className="w-1/4 mx-auto flex flex-col gap-14">
                 <div className="input-container">
+                    <label className="label">Email</label>
                     <input
-                        id="lastName"
-                        name="lastName"
-                        type="text"
+                        id="email"
+                        name="email"
+                        type="email"                            
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
                     />
-                    <label className="label">Last Name</label>
+                    {formik.errors.email ? <div className="text-red-500">{formik.errors.email}</div> : null}
                 </div>
+                
                 <div className="input-container">
+                    <label className="label">Password</label>
                     <input
-                        id="lastName"
-                        name="lastName"
-                        type="text"
+                        id="password"
+                        name="password"
+                        type="text"                        
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
                     />
-                    <label className="label">Last Name</label>
+                    {formik.errors.password ? <div className="text-red-500">{formik.errors.password}</div> : null}
+
                 </div>
-                <div className="input-container">
-                    <input
-                        id="lastName"
-                        name="lastName"
-                        type="text"
-                    />
-                    <label className="label">Last Name</label>
-                </div>
-                <button type="submit">login</button>
+                <button type="submit" className="btn btn-block">login</button>
             </form>
         </div>
     );
