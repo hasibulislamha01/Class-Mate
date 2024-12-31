80b69cd0e6a3834f859d9ecf26add754008bb2b5
hasibulislamha01 <hasibulislamha01@gmail.com>
2025-01-01 00:35:38 +0600

    updated tutor materials tab

--- src/Pages/TutorPages/MySessionPages/MaterialTab.jsx
+++ src/Pages/TutorPages/MySessionPages/MaterialTab.jsx
@@ -1,23 +1,37 @@
+import { Button } from "antd";
 import Info from "./Info";
+
 
 const MaterialTab = () => {
 
     const iterableItems = [
         { itemName: 'Uploaded', itemValue: '2', unit: 'materials' },
-        { itemName: 'Requests', itemValue: '3'},
+        { itemName: 'Requests', itemValue: '3' },
     ]
     return (
-        <div className="grid grid-cols-2 place-items-center justify-items-start gap-x-5">
-            {
-                iterableItems?.map(item =>
-                    <Info
-                        key={item?.itemName}
-                        itemName={item.itemName}
-                        itemValue={item.itemValue}
-                        unit={item.unit || ''}
-                    />
-                )
-            }
+
+        <div>
+
+            <div className="grid grid-cols-1 place-items-center justify-items-start gap-y-4">
+                {
+                    iterableItems?.map(item =>
+                        <Info
+                            key={item?.itemName}
+                            itemName={item.itemName}
+                            itemValue={item.itemValue}
+                            unit={item.unit || ''}
+                        />
+                    )
+                }
+            </div>
+
+            <Button
+                shape="circle"
+            // icon={}
+            >
+                u
+            </Button>
+
         </div>
     );
 };
