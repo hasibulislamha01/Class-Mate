04b0afd44912ee25c40d2f856ec6665ee622cd85
hasibul islam <hasibulislamha01@gmail.com>
2025-01-23 22:23:10 +0600

    updated material card

--- src/Pages/TutorPages/MyMaterialsPage/MaterialsCard.jsx
+++ src/Pages/TutorPages/MyMaterialsPage/MaterialsCard.jsx
@@ -1,13 +1,15 @@
-// import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Card } from 'antd';
 import PropTypes from 'prop-types'
+import { IoIosLink } from 'react-icons/io';
 
 const { Meta } = Card;
 const MaterialsCard = ({ material }) => {
 
     const driveLinkSection =
         <>
-            <p>
+            <p className='flex items-center gap-2'>
+                <IoIosLink />
                 <a href={material?.driveLink}>Browse Drive Link</a>
             </p>
         </>
@@ -17,21 +19,24 @@ const MaterialsCard = ({ material }) => {
                 // width: 300,
             }}
             cover={
-                <img
-                    alt="example"
-                    src={material?.materialImage}
-                />
+                <div className='w-full h-28'>
+                    <img
+                        alt="example"
+                        src={material?.materialImage}
+                        className='w-full h-full object-cover'
+                    />
+                </div>
             }
             actions={[
-                // <SettingOutlined key="setting" />,
-                // <EditOutlined key="edit" />,
-                // <EllipsisOutlined key="ellipsis" />,
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined key="ellipsis" />,
             ]}
         >
             <Meta
                 // avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
                 title={material?.materialtitle}
-                description={ driveLinkSection }
+                description={driveLinkSection}
             />
         </Card>
     )
