3fd657a207d3591db2dd2cafe5eef4edea54999b
hasibul islam <hasibulislamha01@gmail.com>
2025-02-07 00:57:50 +0600

    updated all admins page in admin dashboard

--- src/Pages/AdminPages/AllAdminsPage/AllAdmins.jsx
+++ src/Pages/AdminPages/AllAdminsPage/AllAdmins.jsx
@@ -1,26 +1,59 @@
-import AnimatedPhotoFrame from "../../../Components/FramerMotion/AnimatedPhotoFrame";
+import { Link } from "react-router-dom";
 import useGetAllUsersWithSameAttribute from "../../../CustomHooks/useGetAllUsersWithSameAttribute";
+import ShowTable from "../../../Components/UI/ShowTable/ShowTable";
+import DashboardHeading from "../../../Components/SharedComponents/DashboardComponents/DashboardHeading";
+
+
+const columnItems = [
+    {
+        title: 'Admin Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (title, record) => (
+            <div className='flex items-center gap-3'>
+                <div className='h-8 w-8 '>
+                    <img src={record?.thumbnail} alt="session image" className='h-full w-full rounded-full' />
+                </div>
+                <h3><Link to={`/users/${record.id}`}>{title}</Link></h3>
+            </div>
+        ),
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Phone',
+        dataIndex: 'phone',
+        key: 'phone',
+    },
+]
+
 
 const AllAdmins = () => {
 
-    const admins = useGetAllUsersWithSameAttribute('Administrator', 'all')
+    const admins = useGetAllUsersWithSameAttribute('administrator')
+
+    const tableData = admins?.map((admin, index) => {
+        return (
+            {
+                key: index + 1,
+                id: admin._id,
+                name: admin.userName,
+                thumbnail: admin.userPhoto,
+                email: admin.userEmail,
+                phone: admin.phone
+            }
+        )
+    })
+
     return (
-        <div className="min-h-screen border border-red-400">
-            <h1 className="text-center text-xl font-bold my-6">ClassMate Admins</h1>
+        <div className="min-h-screen">
+            
+            <DashboardHeading title={'ClassMate Admins'} subtitle={'These are the admins who have the power'}/>
 
-            <div className="relative h-full grid grid-cols-1 place-items-center ">
-                {
-                    admins?.map(admin =>
-                        // 
-
-                        <AnimatedPhotoFrame
-                            key={admin._id}
-                            src={admin.userPhoto}
-                            alt={'admin image'}
-                        />
-                    )
-                }
-            </div>
+            <ShowTable columns={columnItems} dataSource={tableData} />
         </div>
     );
 };
