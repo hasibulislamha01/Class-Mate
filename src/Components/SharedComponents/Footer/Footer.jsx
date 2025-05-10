e4aa86b0ae71d5dbb1f2bdbc224679bd98ee1d23
hasibul islam <hasibulislamha01@gmail.com>
2025-05-10 23:53:01 +0600

    footer updated

--- src/Components/SharedComponents/Footer/Footer.jsx
+++ src/Components/SharedComponents/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { RiFacebookCircleLine, RiInstagramLine, RiYoutubeLine } from "react-icons/ri";
+
 
 const Footer = () => {
     return (
@@ -10,14 +11,46 @@ const Footer = () => {
                     <h1 className="text-xl font-semibold">Class<span className="font-semibold text-secondary">Mate</span></h1>
                 </div>
 
-                <div className="flex flex-col items-start">
-                    <h1 className="text-accent font-semibold">Contact</h1>
-                    <p className="font-light"><Link to='' target="_blank">github</Link></p>
-                    <p><Link to='' target="_blank">Linkedin</Link></p>
-                    <p><Link to='' target="_blank">facebook</Link></p>
+                <button className="bg-secondary text-black py-1 px-4 rounded-full font-medium hover:scale-95 transition-all duration-200">Get in touch</button>
+            </section>
+            <hr className="mt-4 border-b border-accent/50" />
+
+            <section className="my-12 grid grid-cols-4">
+
+                <div>
+                    <h4 className="font-bold mb-2">Address</h4>
+                    <div className="text-sm font-light  space-y-1">
+                        <p>6206, Rajshahi City, <br /> Rajshahi,  Bangladesh.</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 className="font-bold  mb-2">Contact Us</h4>
+                    <div className="text-sm font-light mb-2 space-y-1">
+                        <p>+880 1754 XXX</p>
+                        <p>help@classmate.com</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 className="font-bold mb-2">Services</h4>
+                    <div className="text-sm font-light space-y-1">
+                        <p className="text-sm font-light">Consultation</p>
+                        <p className="text-sm font-light">Career Guide</p>
+                        <p className="text-sm font-light">Formal Exams support</p>
+                        <p className="text-sm font-light">Technical issue</p>
+                        <p className="text-sm font-light">Admission support</p>
+                    </div>
+                </div>
+
+                <div>
+                    <h1 className="font-bold mb-2">Social</h1>
+
+                    <div className="flex items-center gap-5">
+                        <RiFacebookCircleLine size={20}/>
+                        <RiInstagramLine size={20} />
+                        <RiYoutubeLine size={20} />
+                    </div>
                 </div>
             </section>
-            <hr className="border-b border-accent/50" />
 
         </footer>
     );
